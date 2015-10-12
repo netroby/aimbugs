d4806664d730d2fa3128e1cf3671d4fe5f7317a9
ZhiFeng Hu <hufeng1987@gmail.com>
2015-10-13 07:54:20 +0800

    Update PHPVersionTest
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

---
+++ tests/PHPVersionTest.php
@@ -0,0 +1,10 @@
+<?php
+class HelloWorldTest extends PHPUnit_Framework_TestCase
+{
+    public function testHelloWorld()
+    {
+        $version = phpversion();
+        echo ' The php version is: '.$version;
+        $this->assertNotEmpty($version);
+    }
+}
